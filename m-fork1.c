290a7dad24c1027e6feed2cd10259876c9ecaef3
Vu Nguyen <vung0724@gmail.com>
2019-06-19 21:53:00 -0400

    using fork

---
+++ m-fork1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int i;
+  int n = 4;
+  
+  int childpid;
+
+  for (i = 0; i < n ; i++)
+  {
+
+      childpid = fork();
+      if(childpid == 0)
+         fprintf(stderr,"i = %d, childpid = %d. This is process %ld with child %ld\n",i,childpid,(long)getpid(),(long)getpid());
+      else break;
+  }
+  fprintf(stderr,"i = %d , childpid = %d . This is process %ld with parent %ld\n",i,childpid,
+  (long)getpid(),(long)getpid());
+
+  return 0;
+}
